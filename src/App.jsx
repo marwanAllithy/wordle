034c85429d636fa927448a451ae5c00a4f266961
OZY <92400495+OZY3112@users.noreply.github.com>
2022-04-22 23:10:51 +0200

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 function App() {
-
+  const [solution, setSolution] = useState(null);
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+    .then((res) => res.json())
+    .then(json => {
+      // random int between 1  and 14
+      const randomSolution = json[ Math.floor(Math.random() * json.length)]
+      setSolution(randomSolution)
+      console.log(json)
+    })
+  }, [setSolution])
   return (
     <div className="App">
       
